fc3dbaccf9834f2981ba90f0c954d7328b119dda
guyusuye1105 <761266430@qq.com>
2019-07-01 13:30:43 +0800

    no message

--- mall/app/default/siteinfo.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  name: "小程序商城",
- // siteroot: "https://mp.minstech.cn/addons/mall/web/", // 必填: api地址
-  siteroot: "http://10.131.4.200:9001/addons/mall/web/",
- // siteroot: "http://wx.cn/addons/mall/web/", // 必填: api地址
-  // siteroot: 'https://yanshi.yiovo.com/',
-  uniacid: "16", // 此处为商城ID, 可在超管后台-商城列表中查看
-};
